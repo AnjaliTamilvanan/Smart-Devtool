0e5ef432313dfa7fd4351cac8927d124f3d8ce23
AnjaliTamilvanan <anjalitamilvanan2002@gmail.com>
2026-04-15 20:04:32 +0530

    Created result page with backend integration

---
+++ api-devtool/frontend/src/pages/Result.jsx
@@ -0,0 +1,39 @@
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { analyzeAPI } from "../services/api";
+import Loader from "../components/Loader";
+import "../styles/main.css";
+
+function Result() {
+  const location = useLocation();
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    analyzeAPI(location.state)
+      .then((res) => {
+        setResult(res);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <Loader />;
+
+  return (
+    <div className="container">
+      <h2>📊 Extracted API Info</h2>
+      <pre className="box">
+        {JSON.stringify(result.structured_data, null, 2)}
+      </pre>
+
+      <h2>💻 Generated Code</h2>
+      <pre className="box">{result.generated_code}</pre>
+    </div>
+  );
+}
+
+export default Result;
