0e2a949fa51c3b3af179b8adafd319dab2f3cf40
AnjaliTamilvanan <anjalitamilvanan2002@gmail.com>
2026-04-15 21:16:44 +0530

    Fixed CORS issue to allow frontend-backend communication

--- api-devtool/backend/main.py
+++ api-devtool/backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from routes.api_tool import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all (for now)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
